3f7dcc4b5dde760082c83ac453ad19aecde2729c
Luanna Delicato <ldelicato@outlook.com>
2023-05-20 12:01:20 +0100

    Initial commit

---
+++ api/config.php
@@ -0,0 +1,28 @@
+<?php
+$envContent = file_get_contents('.env');
+$env = explode("\n", $envContent);
+
+foreach ($env as $data) {
+    $data = trim($data);
+
+    if (!empty($data) && $data[0] !== '#') {
+        list($envVariable, $envValue) = explode('=', $data, 2);
+        $envVariable = trim($envVariable);
+        $envValue = trim($envValue);
+
+        putenv("$envVariable=$envValue");
+    }
+}
+
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_DATABASE');
+$dbUsername = getenv('DB_USERNAME');
+$dbPassword = getenv('DB_PASSWORD');
+
+try {
+    $db = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4", $dbUsername, $dbPassword);
+} catch (PDOException $err) {
+    die("Conexão falhou: " . $err->getMessage());
+}
+
+
